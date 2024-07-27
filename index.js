8cc2eefe264a489c078fc66905747f6a0da1b92e
Sky Marsh <soras1758@gmail.com>
2024-07-28 07:41:30 +0900

    Add function to store book info

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ function Book(title, author, pages, read){
     this.pages=pages;
     this.read=read;
     this.info = function(){
-        function readOrNot(){}
-        let bookInfo = this.title + "by" + this.author +","+ this.pages + "," 
+        let readOrNot;
+        if(this.read){
+            readOrNot="read";
+        }else{
+            readOrNot="not read yet";
+        }            
+
+        let bookInfo = this.title + " by " + this.author +", "+ this.pages + "pages," + readOrNot +'.';
+        return bookInfo;
     }
 };
+    const book1 = new Book('Harry Potter', "JK Rowling", '800', false);
+
+    console.log(book1.info());
