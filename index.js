c39e79e2f2a523afc06ac8892cd99891afe5b696
Sky Marsh <soras1758@gmail.com>
2024-08-02 07:54:22 +0900

    Add function to delete selected book.
    Add delete button to each book.
    Associated each book's index with data attribute. When delete button is clicked,
    it will return book's index.
    querySelector with the clicked parent's element book index.
    Remove the entire element with remove() method.

--- index.js
+++ index.js
@@ -59,12 +59,13 @@ const mainContainer = document.querySelector('main');
 
 function displayBook() {
     mainContainer.innerHTML = "";
-
-    myLibrary.forEach((e) => {
+    
+    myLibrary.forEach((e,index) => {
         const cardContainer = document.createElement('div');
+        cardContainer.setAttribute("data-index",index);
         cardContainer.classList.add("cardContainer");
         mainContainer.append(cardContainer);
-
+        
         const bookSvg = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
         bookSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
         bookSvg.setAttribute("xmlns:xlink", "http://www.w3.org/1999/xlink");
@@ -74,14 +75,15 @@ function displayBook() {
         bookSvg.setAttribute("height", "100%");
         bookSvg.setAttribute("viewBox", "0 0 459.319 459.319");
         bookSvg.setAttribute("xml:space", "preserve");
-
+        
         // Set the fill attribute with a random color
         bookSvg.setAttribute("fill", `#${generateRandomColor()}`);
-
+        
         const g = document.createElementNS("http://www.w3.org/2000/svg", "g");
-
+        
         const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
         path.setAttribute("d", "M94.924,366.674h312.874c0.958,0,1.886-0.136,2.778-0.349c0.071,0,0.13,0.012,0.201,0.012 c6.679,0,12.105-5.42,12.105-12.104V12.105C422.883,5.423,417.456,0,410.777,0h-2.955H114.284H94.941 c-32.22,0-58.428,26.214-58.428,58.425c0,0.432,0.085,0.842,0.127,1.259c-0.042,29.755-0.411,303.166-0.042,339.109 c-0.023,0.703-0.109,1.389-0.109,2.099c0,30.973,24.252,56.329,54.757,58.245c0.612,0.094,1.212,0.183,1.847,0.183h317.683 c6.679,0,12.105-5.42,12.105-12.105v-45.565c0-6.68-5.427-12.105-12.105-12.105s-12.105,5.426-12.105,12.105v33.461H94.924 c-18.395,0-33.411-14.605-34.149-32.817c0.018-0.325,0.077-0.632,0.071-0.963c-0.012-0.532-0.03-1.359-0.042-2.459 C61.862,380.948,76.739,366.674,94.924,366.674z M103.178,58.425c0-6.682,5.423-12.105,12.105-12.105s12.105,5.423,12.105,12.105 V304.31c0,6.679-5.423,12.105-12.105,12.105s-12.105-5.427-12.105-12.105V58.425z");
+        const deleteBtn = document.createElement('button');
 
         g.appendChild(path);
         bookSvg.appendChild(g);
@@ -96,8 +98,9 @@ function displayBook() {
         author.textContent = `Written by ${e.author}`;
         pages.textContent = e.pages + " pages";
         read.textContent = e.read ? "Status: Read" : "Status: Unread";
+        deleteBtn.textContent = "Delete";
 
-        cardContainer.append(title, author, pages, read);
+        cardContainer.append(title, author, pages, read,deleteBtn);
 
         const cardInfo = cardContainer.querySelectorAll('div');
         cardInfo.forEach((element) => {
@@ -110,6 +113,19 @@ displayBook();
 const dialog = document.querySelector("dialog");
 const modalBtn = document.querySelector("button");
 
+const closeBtn = 
 modalBtn.addEventListener("click", ()=>{
     dialog.showModal();
 });
+
+const deleteBtns = document.querySelectorAll("button");
+
+//get index by accessing parent's dataset. 
+deleteBtns.forEach(deleteBtn=>{
+    deleteBtn.addEventListener("click",e=>{
+        let parentIndex= e.target.parentElement.dataset.index;
+        const targetElement = document.querySelector(`[data-index='${parentIndex}']`);
+        targetElement.remove();
+    });
+});
+
